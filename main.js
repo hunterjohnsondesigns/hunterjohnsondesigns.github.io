6751201b48f3097fb6d1bd07ec13c9017c7ec3bc
hunterjohnsondesigns <hunterjohnsongd@gmail.com>
2023-04-15 19:56:15 -0400

    slider test

--- main.js
+++ main.js
@@ -29,6 +29,10 @@ $('#menu').onclick=()=>{              $('#menu').classList.toggle('rotate')
 }
 
 
+
+
+
+
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
   var x = document.getElementById("myLinks");
@@ -37,4 +41,38 @@ function myFunction() {
   } else {
     x.style.display = "block";
   }
+}
+
+
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
