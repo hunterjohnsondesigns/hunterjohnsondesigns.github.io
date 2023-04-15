d2121dcb55b950b6161001194d31f27a56c2c1c9
hunterjohnsondesigns <hunterjohnsongd@gmail.com>
2023-04-15 17:05:12 -0400

    hamburger mobile nav experiment

--- main.js
+++ main.js
@@ -28,3 +28,13 @@ $('#menu').onclick=()=>{              $('#menu').classList.toggle('rotate')
 }
 }
 
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
